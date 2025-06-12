3b3d514d89042d6e0727e00bee34fced921ee00d
Your Name <your.email@example.com>
2025-06-12 12:14:38 -0400

    Completed task

--- src/ShipCostCalculator.java
+++ src/ShipCostCalculator.java
@@ -1,15 +1,36 @@
+import java.util.Scanner;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
+public class ShipCostCalculator {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
-
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+        Scanner in = new Scanner(System.in);
+        double itemPrice = 0;
+        final double SHIPPING_TAX = .02;
+        double shippingPrice = 0;
+        String trash = "";
+        System.out.print("What is the item price? $");
+        if(in.hasNextDouble())
+        {
+            itemPrice = in.nextDouble();
+            in.nextLine();
+            if(itemPrice >= 100)
+            {
+                shippingPrice = 0;
+                System.out.println( "The shipping price is $" + shippingPrice + " and the total price is $" + (itemPrice + shippingPrice) );
+            }
+            else
+            {
+                shippingPrice = itemPrice * SHIPPING_TAX;
+                itemPrice += shippingPrice;
+                System.out.println( "The shipping price is " + shippingPrice + " and the total price is " + itemPrice );
+            }
+        }
+        else
+        {
+            trash = in.nextLine();
+            System.out.println( "You did not enter a valid number: "  + trash);
+            System.out.println("Run the program agian and enter a valid number.");
         }
     }
 }
